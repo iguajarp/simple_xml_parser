a84ee40c81a377d0c88642df35825d7a665be402
Italo Guajardo <italog.penaloza@gmail.com>
2023-01-13 12:27:20 -0300

    complete simple parser. wip: fix two final tests

--- src/lib.rs
+++ src/lib.rs
@@ -489,3 +489,45 @@ fn single_element_parser() {
         single_element().parse("<div class=\"float\"/>")
     )
 }
+
+#[test]
+fn xml_parser() {
+    let doc = r#"
+        <top label="Top">
+        <semi-bottom label="Bottom"/>
+            <middle>
+                <bottom label="Another bottom"/>
+            </middle>
+        </top>"#;
+    let parsed_doc = Element {
+        name: "top".to_string(),
+        attributes: vec![("label".to_string(), "Top".to_string())],
+        children: vec![
+            Element {
+                name: "semi-bottom".to_string(),
+                attributes: vec![("label".to_string(), "Bottom".to_string())],
+                children: vec![],
+            },
+            Element {
+                name: "middle".to_string(),
+                attributes: vec![],
+                children: vec![Element {
+                    name: "bottom".to_string(),
+                    attributes: vec![("label".to_string(), "Another bottom".to_string())],
+                    children: vec![],
+                }],
+            },
+        ],
+    };
+
+    assert_eq!(Ok(("", parsed_doc)), element().parse(doc))
+}
+
+#[test]
+fn mismatched_closing_tag() {
+    let doc = r#"
+        <top>
+            <bottom/>
+        </middle>"#;
+    assert_eq!(Err("</middle>"), element().parse(doc));
+}
